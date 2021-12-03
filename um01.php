b1b7004b334877fbc33823b988fb88bf30643de3
harikumar0s <91002373+harikumar0s@users.noreply.github.com>
2021-12-03 17:43:57 +0530

    Update um01.php

--- um01.php
+++ um01.php
@@ -10,12 +10,11 @@
   article{border: 3px inset rgb(209, 192, 192); border-radius: 25px; position: relative; top: 50px; width: fit-content;padding-left: 60px;padding-right: 60px;}
   </style>
 </head>
-<body>    <center>
-  <article>
+<body>    
+  <center>
   <img src="clipart543897.png" width="300" height="250">
-   <pre><h2 style="font-size:30px;">This website is currenlty under maintenance!
+  <pre><h2 style="font-size:30px;">This website is currenlty under maintenance!
     come back later :)</h2></pre>
-  
-</article></center>
+  </center>
 </body>
 </html>
